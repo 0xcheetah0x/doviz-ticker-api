f4df26bb0ba054433171a98e67360a497eab2736
0xcheetah0x <0xcheetah0x@gmail.com>
2025-09-10 13:16:27 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+app = FastAPI()
+
+# CORS açıyoruz (OBS tarayıcı kaynağı rahat erişsin diye)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/doviz")
+def doviz():
+    try:
+        # USD & EUR verisi
+        fx = requests.get("https://open.er-api.com/v6/latest/USD").json()
+        usdtry = round(fx["rates"]["TRY"], 2)
+        eurtry = round(fx["rates"]["TRY"] / fx["rates"]["EUR"], 2)
+
+        # Bitcoin verisi
+        btc = requests.get("https://api.coindesk.com/v1/bpi/currentprice/USD.json").json()
+        btcusd = round(btc["bpi"]["USD"]["rate_float"], 0)
+
+        return {
+            "usdtry": usdtry,
+            "eurtry": eurtry,
+            "btcusd": btcusd
+        }
+    except Exception as e:
+        return {"error": str(e)}
